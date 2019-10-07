5c3c6f870c5d5b0c46593ba3ca3f136282e90486
Pablo Escobar Lopez <pescobar001@gmail.com>
2019-10-07 16:01:23 +0200

    fix typo in wrapper

--- libbi_wrapper.sh
+++ libbi_wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rename this script to "libbi" and add it to your PATH
-singularity /path/to/container/libbi-1.4.5.simg libbi "$@"
+singularity exec /path/to/container/libbi-1.4.5.simg libbi "$@"
